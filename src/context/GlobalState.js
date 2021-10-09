74d6583d641d15749c082fb2fe0c0e276b848e63
Shubham <shubh.chavhan00@gmail.com>
2021-10-09 22:55:36 +0530

    added function to add  or delete transactions

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,15 +1,9 @@
 import React, { createContext, useReducer } from "react";
-
 import AppReducer from "./AppReducer";
 
 //Intial state
 const initialState = {
-  transactions: [
-    { id: 1, text: "Flower", amount: -20 },
-    { id: 2, text: "Salary", amount: 300 },
-    { id: 3, text: "Book", amount: -10 },
-    { id: 4, text: "Camera", amount: 150 },
-  ],
+  transactions: [],
 };
 
 //create context
@@ -19,8 +13,27 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  }
+  function addTransaction(transation) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transation,
+    });
+  }
+
   return (
-    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
       {children}
     </GlobalContext.Provider>
   );
