770358a858e3bb2d022bf3580f3053414bdc2223
Shubham <shubh.chavhan00@gmail.com>
2021-10-09 19:41:43 +0530

    Wrapped all the components inside the GlobalProvider

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import { Balance } from "./components/Balance";
 import { IncomeExp } from "./components/IncomeExp";
 import { TransactionList } from "./components/TransactionList";
 import { AddTransaction } from "./components/AddTransaction";
+import { GlobalProvider } from "./context/GlobalState";
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
@@ -16,7 +17,7 @@ function App() {
         <TransactionList />
         <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
