9f012450e44d70e821d84b64662d78f49cf02d4e
Shubham <shubh.chavhan00@gmail.com>
2021-10-10 09:52:16 +0530

    added Github icon which linked to Github repositories

--- src/App.js
+++ src/App.js
@@ -10,6 +10,13 @@ import { GlobalProvider } from "./context/GlobalState";
 function App() {
   return (
     <GlobalProvider>
+      <a
+        href="https://github.com/ShubhamChavhan09/expense-tracker"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <i className="fab fa-github"></i>
+      </a>
       <Header />
       <div className="container">
         <Balance />
